4482add0ca9ba53061fd4dd3bdea997484c6486d
ShaikhZai <shaikhmoh1173@gmail.com>
2023-01-15 16:17:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
+import backgroundVideo from './image/vid1.mp4'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    advice: '',
+  }
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        const { advice } = response.data.slip; //advice fromresponse.data.slip in the api 
+
+        this.setState({ advice });// in js a=a can be written as a itself
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <video autoPlay playsInline muted loop id="video">
+            <source src={backgroundVideo} type='video/mp4'/>
+        </video>
+        <div className="card">
+          <h1 className="heading">{this.state.advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>GIVE ME AN ADVIZE!</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
